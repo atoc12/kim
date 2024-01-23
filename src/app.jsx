d307ec7534ddc523ebf10697fb0e1f7f4a69ec94
atoc12 <octaviosdf1@gmail.com>
2024-01-23 20:28:21 -0300

    para kim :p

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import { LayoutPrimary } from "./components/layout1/layout_grid"
+import './index.css';
+
+export const App = ()=>{
+    return(
+        <>
+            <header>
+
+            </header>
+            <main>
+                <LayoutPrimary/>
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    )
+}
---
+++ src/components/layout1/layout_grid.jsx
@@ -0,0 +1,58 @@
+import './layout.css';
+export const LayoutPrimary = ()=>{
+    return(
+        <div id="layout_primary">
+            <section>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199488123021824112/IMG-20240123-WA0048.jpg?ex=65c2b95d&is=65b0445d&hm=9e103b71c492155739a308de73050ececef3dc288b0cd8ab3727f645d7a14db8&=&format=webp&width=507&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199488970703249479/20240123_195745.jpg?ex=65c2ba28&is=65b04528&hm=6d172ebd5646176034a5b6a7e4e63d222587cc31fcecdb41e5dd74f8f27f3afd&=&format=webp&width=312&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199488972238377050/IMG-20240122-WA0020.jpg?ex=65c2ba28&is=65b04528&hm=df840b95cb55f5d98f3ae9cbae169038957d594443b790a2ebfc925b7db30fb0&=&format=webp&width=380&height=675" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199488971714076895/20240123_164203.jpg?ex=65c2ba28&is=65b04528&hm=c1b2f5639cf20fed170016bc54b186e1305657275f666e9ba26b4b36d8bdb856&=&format=webp&width=312&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://cdn.discordapp.com/attachments/1199364702736752702/1199488971202363402/IMG-20240123-WA0050.jpg?ex=65c2ba28&is=65b04528&hm=85710089218cceaf8f2b5590ab0bdc886eefe988ae78b8d32cd0ee008751a072&" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199488973404381234/IMG_20240121_123746_097.webp?ex=65c2ba28&is=65b04528&hm=64c5de8b304dc3cf47b7d68cc76094dab6b72a867b10e25b3216ba781b40a649&=&format=webp&width=380&height=675" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199492932688945275/IMG-20240117-WA0055.jpg?ex=65c2bdd8&is=65b048d8&hm=0e4b6a3751d1cab6cbc3fa3b98b118b5dd7df78a5f5bdad234571794701b2441&=&format=webp&width=507&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199493110137356348/IMG-20240112-WA0032.jpg?ex=65c2be02&is=65b04902&hm=ca3dd00105c26b0eaedde576b8c49642663097943111a4b6b6ff34cb0de7d486&=&format=webp&width=507&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199493109864747208/20240111_143311.jpg?ex=65c2be02&is=65b04902&hm=dafd63702b98603b9da6776283025847a9d4d04934f0f32df4f60e75afdfac71&=&format=webp&width=1068&height=676" alt="" />
+                </div>
+            </section>
+            <section>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199488972854919278/IMG-20240121-WA0050.jpg?ex=65c2ba28&is=65b04528&hm=9e14e307103b408dca26ef4f6042b28161791d362cf8135a1452d9cc6a503fdc&=&format=webp&width=507&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199493109315272845/IMG-20240109-WA0000.jpg?ex=65c2be02&is=65b04902&hm=83fd281c2658ee0a523a8a02688d5b5e52b1e1a672cc455a4b1bd82862efd1f0&=&format=webp&width=507&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199493728256131123/Screenshot_20240123-201935_Photos.jpg?ex=65c2be96&is=65b04996&hm=f4fa3e49e054222d63739ffa5815e7dff628ae4ab2ef0cb3a981a4ff2dee9556&=&format=webp&width=312&height=676" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199493109315272845/IMG-20240109-WA0000.jpg?ex=65c2be02&is=65b04902&hm=83fd281c2658ee0a523a8a02688d5b5e52b1e1a672cc455a4b1bd82862efd1f0&=&format=webp&width=507&height=676" alt="" />
+                </div>
+            </section>
+            <section>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199494357397544970/IMG-20221128-WA0018.jpg?ex=65c2bf2c&is=65b04a2c&hm=adb085cb841f6c744f16a38902b24917a7dc0ab4326f38bf4c3bee29c3ba14e3&=&format=webp&width=380&height=675" alt="" />
+                </div>
+                <div>
+                    <img src="https://media.discordapp.net/attachments/1199364702736752702/1199494457456857148/20231022_170229.jpg?ex=65c2bf44&is=65b04a44&hm=9c3825ecf4657da2e8fedec65e903da5f7b3e8fcee066803ee450c22e7ff7521&=&format=webp&width=1440&height=664" alt="" />
+                </div>
+            </section>
+        </div>
+    )
+}
